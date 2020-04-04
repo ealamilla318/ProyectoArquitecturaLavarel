4ab3a944f2495b529e0757bbe24d00c094fca9a1
wPony <58406272+wPony@users.noreply.github.com>
2020-04-03 23:43:08 -0600

    Tabla_Inventario

--- resources/views/TablaEjemplo.blade.php
+++ resources/views/TablaEjemplo.blade.php
@@ -1,32 +1,32 @@
 @extends('layouts.MasterLayout')
-@section('title', 'InventarioAAAAA')
+@section('title', 'InventarioJitomates')
 @section('content')
 
 <main class="page">
         <section class="clean-block slider">
         <div class="container">
-        <a href="{{route('insertar')}}" type="button" class="btn btn-sucess" >Insertar</a>
+            <a href="{{route('insertar')}}" type="button" class="btn btn-outline-secondary" style="margin-bottom: 10px; width: 150px; position: relative; left: 85%;">Insertar</a>
                 <div class="table-responsive">
                     <table class="table">
                         <thead>
                             <tr>
                             <th>id</th>
-                            <th>tipo</th>
-                            <th>caja</th>
+                            <th>Tipo</th>
+                            <th>Caja</th>
                             <th>Fecha</th>
+                            <th></th>
+                            <th></th>
                             </tr>
                         </thead>
                         @foreach($crudes as $crude)
                         <tbody>
-                       
                             <tr>
                             <td>{{$crude->id}}</td>
                             <td>{{$crude->tipo}}</td>
                             <td>{{$crude->caja}}</td>
                             <td>{{ \FormatTime::LongTimeFilter($crude->created_at) }}</td>
-                            <td><a href="{{url('/editar/'.$crude->id)}}" type="button" class="btn btn-sucess" >Editar </a></td>
-                            <td><a href="{{url('/delete/'.$crude->id)}}" type="button" class="btn btn-sucess" >Eliminar </a></td>
-                            </tr>1
+                            <td><a href="{{url('/editar/'.$crude->id)}}" type="button" class="btn btn-outline-dark">Editar </a><a href="{{url('/delete/'.$crude->id)}}" type="button" class="btn btn-outline-dark" style="margin-left: 8px;">Eliminar </a></td>
+                            </tr>
                             
                         
                         </tbody>
